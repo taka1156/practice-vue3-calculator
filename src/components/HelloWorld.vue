c555208ecb9e92afd9be414545b65a8389ae4953
taka <tomo05061@gmail.com>
2020-10-06 11:56:54 +0900

    1st commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -49,9 +49,7 @@
         <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
       </li>
       <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a>
       </li>
       <li>
         <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
@@ -91,10 +89,7 @@
         >
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener"
           >awesome-vue</a
         >
       </li>
@@ -103,10 +98,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String
   }
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,18 @@
-declare module "*.vue" {
-  import { defineComponent } from "vue";
+// https://github.com/vuejs/vue-cli/issues/1198#issuecomment-636468006
+declare module '*.vue' {
+  import { defineComponent } from 'vue';
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
+}
+
+declare module '@/views/*' {
+  import { defineComponent } from 'vue';
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
+}
+
+declare module '@/components/*' {
+  import { defineComponent } from 'vue';
   const component: ReturnType<typeof defineComponent>;
   export default component;
 }
