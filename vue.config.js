45110aba2f5d5ce90a19ac9c0a92e07478022cff
taka <tomo05061@gmail.com>
2020-10-06 16:10:56 +0900

    fix: ビルド設定の修正

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  // publicPath: './'
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/practice-vue3-calculator/' : '/',
   outputDir: 'docs',
-  assetsDir: './',
-  publicPath: './'
+  assetsDir: './'
 };
