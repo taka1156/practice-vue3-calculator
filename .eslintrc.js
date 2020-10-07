1d4673b4908699ceb6231e2a100197cf7ecf4d83
taka <tomo05061@gmail.com>
2020-10-07 16:35:41 +0900

    fix: logic見直し

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,5 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "@typescript-eslint/explicit-function-return-type":["error"]
   }
 };
--- src/types/cal-type.d.ts
+++ src/types/cal-type.d.ts
@@ -1,2 +1,5 @@
-type keyCode = 'clear' | 'cal' | 'ope'| 'ope,cal' | 'reverse' | 'num';
+type keyCodeType = 'clear' | 'cal' | 'ope' | 'ope,cal' | 'reverse' | 'num';
 
+type calIndex = 0 | 1;
+
+type calAns = number | string;
